0e632b766e60f0e3b76e403de3511048b683acd6
militoj <militoj@gmail.com>
2017-03-09 18:33:06 -0600

    added method to CheeseController and initiated html template

--- src/main/java/com/example/controllers/CheeseController.java
+++ src/main/java/com/example/controllers/CheeseController.java
@@ -1,7 +1,20 @@
 package com.example.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by johnmilito on 3/9/17.
  */
+
+@Controller
 public class CheeseController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "My Cheese";
+
+    }
 }
