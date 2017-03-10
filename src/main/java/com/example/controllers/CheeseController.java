acfcff96dc021a3870cdd27891858ed76d5369cf
militoj <militoj@gmail.com>
2017-03-09 19:47:47 -0600

    used thymeleaf to iterate over ArrayList to populate an html list

--- src/main/java/com/example/controllers/CheeseController.java
+++ src/main/java/com/example/controllers/CheeseController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 /**
  * Created by johnmilito on 3/9/17.
  */
@@ -15,6 +17,14 @@ public class CheeseController {
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        ArrayList<String> cheeses = new ArrayList<>();
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("munster");
+
+        model.addAttribute("cheeses", cheeses);
+
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
